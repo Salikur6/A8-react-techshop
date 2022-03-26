97992fdb82795a30fdda443c6d77e798b406c989
salikur6 <salikur6@gmail.com>
2022-03-26 23:23:25 +0600

    choose and reset btn set

--- src/components/CartDetails/CartDetails.js
+++ src/components/CartDetails/CartDetails.js
@@ -2,33 +2,41 @@ import React from 'react';
 
 const CartDetails = ({carts}) => {
     
-    const name =[];
-    const img = [];
+    // const name =[];
+    // const img = [];
     
-    for(const proparty of carts){
-        name.push(proparty.name);
-        img.push(proparty.img);
-        // console.log(proparty)
-       
-    }
+    // // for(const proparty of carts){
+    // //     name.push(proparty.name);
+    // //     img.push(proparty.img);
+    // //     // console.log(proparty)
+    // // }
+
+    
     return (
         <div>
             <h2 className='fw-bold'>Selected Items {carts.length}</h2>
           
           {
-              carts.map(cart => <SingleCart key={cart.id} cart={cart}></SingleCart>)
+              carts.map(cart => <SingleCart key={cart.id} cart={cart}></SingleCart>)  
           }
+
+
+
         </div>
     );
 };
 
 
 const SingleCart = ({cart}) => {
-    console.log(cart)
+
+    
+
+
+
         return (
          
          <div className='d-flex mt-3'>
-            <img className='border rounded-circle' width='75px' src={cart.img} alt="" />
+            <img className='border rounded-pill' width='75px' src={cart.img} alt="" />
              <p className='fw-bold'>{cart.name}</p>
          </div>
             
--- src/components/LaptopCards/LaptopCards.js
+++ src/components/LaptopCards/LaptopCards.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 import CartDetails from '../CartDetails/CartDetails';
 import SingleCard from '../SingleCard/SingleCard';
 import './LaptopCard.css'
@@ -9,12 +10,46 @@ const LaptopCards = () => {
 
 
     const cartHandler = (card) => {
-        const newCarts = [...carts, card]
+        let newCarts = []
+         newCarts = [...carts, card]
         
+        const exists = carts.find(product => product.id === card.id)
+        if (!exists) {
+          newCarts = [...carts, card]
+        }
+        else {
+          alert('You all ready add this watch')
+          
+        }
+
+        if(newCarts.length <= 4){
+            setCarts(newCarts);
+        }else{
+            alert('oops',)
+           setCarts(newCarts);
+        }
 
          setCarts(newCarts);
     }
     
+    const resetBtn =(card) =>{
+        setCarts([]); 
+        
+    };
+
+    const ChooseBtn =(card) =>{
+        
+        const random = card[Math.floor(Math.random()*card.length)]
+        if(random){
+            // setCarts([])
+            setCarts([random])
+        }
+    }
+
+
+   
+
+
     useEffect( () => {
         fetch('items.json')
         .then(response => response.json())
@@ -33,6 +68,9 @@ const LaptopCards = () => {
             <div className="cart-container col-12 col-lg-3 col-md-4 bg-info">
                 <div className='cart-info'>
                     <CartDetails carts={carts}></CartDetails>
+                    
+                    <button onClick={()=>ChooseBtn(carts)}>Choose 1 For Me</button>
+                    <button onClick={()=>resetBtn(carts)}>Reset</button>
                 </div>
             </div>
         </div>
